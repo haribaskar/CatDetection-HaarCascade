e101ee61ea6afab1e9cb0ade241aec8ec6754201
Hari Baskar <hari.baskar@happiestminds.com>
2017-09-01 16:01:38 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# CatDetection
+Detecting cats in images with OpenCV
+
+## Getting Started
+
+### Prerequisites
+
+What things you need to install:
+
+* Python 
+* OpenCV 
+
+## To Run 
+
+1. Clone this repository:
+```
+git clone https://github.com/haribaskar/CatDetection-HaarCascade.git
+```
+
+2. Run:
+```
+python cat_detector.py -i images/cat_04.jpg
+```
+
+
+
+## Result
+![](https://github.com/haribaskar/CatDetection-HaarCascade/blob/master/result.jpg)
+
+
+
---
+++ cat_detector.py
@@ -0,0 +1,36 @@
+# USAGE
+# python cat_detector.py --image images/cat_01.jpg
+
+# import the necessary packages
+import argparse
+import cv2
+ 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+ap.add_argument("-c", "--cascade",
+	default="haarcascade_frontalcatface.xml",
+	help="path to cat detector haar cascade")
+args = vars(ap.parse_args())
+
+# load the input image and convert it to grayscale
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# load the cat detector Haar cascade, then detect cat faces in the input image
+detector = cv2.CascadeClassifier(args["cascade"])
+rects = detector.detectMultiScale(gray, scaleFactor=1.3,
+	minNeighbors=10, minSize=(75, 75))
+
+# loop over the cat faces and draw a rectangle surrounding each
+for (i, (x, y, w, h)) in enumerate(rects):
+	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+	cv2.putText(image, "Cat #{}".format(i + 1), (x, y - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+#write the image
+cv2.imwrite('result.jpg', image)
+# show the detected cat faces
+cv2.imshow("Cat Faces", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
